a321d2ddd11d39461a4ccd78b365fe6ddc2e76a4
井上隼英 <inoueshunei@inoueshuneinoMacBook-Pro-2.local>
2020-09-04 17:01:40 +0900

    add react-native-swipe-list-view

--- src/components/todoList/card.tsx
+++ src/components/todoList/card.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { CheckBox } from "react-native-elements";
+import { SwipeRow } from 'react-native-swipe-list-view';
 
 import { listWidth } from '../styles';
 
@@ -9,25 +10,36 @@ export interface CardProps {
   isDone: boolean;
 }
 
-const Card: FC<CardProps> = ({ title = "タスクの名前", isDone = false }) => {
+const Card: FC<CardProps> = ({ title = "", isDone = false }) => {
   return (
-    <CheckBox
-      checked={isDone}
-      size={18}
-      title={
-        <View>
-          <Text style={styles.cardTitle}>{title}</Text>
-        </View>
-      }
-      textStyle={styles.cardTitle}
-      containerStyle={styles.cardContainer}
-    />
+    <SwipeRow rightOpenValue={-75} disableRightSwipe={true} style={styles.cardContainer}>
+      <View style={styles.standaloneRowBack}>
+        <Text style={styles.backTextWhite}>Right</Text>
+      </View>
+      <CheckBox
+        checked={isDone}
+        size={18}
+        title={
+          <View>
+            <Text style={styles.cardTitle}>{title}</Text>
+          </View>
+        }
+        textStyle={styles.cardTitle}
+        containerStyle={styles.cardContainer}
+      />
+    </SwipeRow>
   );
 };
 
 export const cardHeight = 64
 
 const styles = StyleSheet.create({
+  container: {
+    // width: listWidth,
+    height: cardHeight,
+    justifyContent: "center",
+    alignContent: 'center',
+  },
   cardContainer: {
     width: listWidth,
     height: cardHeight,
@@ -46,6 +58,21 @@ const styles = StyleSheet.create({
     fontSize: 15,
     marginLeft: 16,
   },
+  standaloneRowBack: {
+    width: listWidth,
+    height: cardHeight,
+    borderRadius: 16,
+    alignItems: 'center',
+    // backgroundColor: 'gray',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+  },
+  backTextWhite: {
+    color: '#FFF',
+  },
+
 });
 
 export default Card;
